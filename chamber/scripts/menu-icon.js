e1061c9abb163be2ab5cee9e16a092516467c595
landesud <landesud@outlook.com>
2024-10-19 15:07:35 -0600

    Chamber Update

--- chamber/scripts/menu-icon.js
+++ chamber/scripts/menu-icon.js
@@ -1,11 +1,31 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.nav-links');
+const body = document.querySelector("body");
+const sections = document.querySelectorAll("section");
+
+const darkMode = document.querySelector('.switch input');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
 
-const article = document.querySelector("article");
-article.innerHTML = 'innerHTML supports <strong>HTML</strong> tags. The textContet property does not';
 
+darkMode.addEventListener('change', function() {
+  if (darkMode.checked) {
+    console.log("Executed");
+    body.style.backgroundColor = "#333";
+    body.style.color = "#fff";
+    sections.forEach(section => {
+      section.style.backgroundColor = "var(--black)";
+      section.style.color = "var(--gray)";
+    });
+  } else {
+    body.style.backgroundColor = "";
+    body.style.color = "";
+    sections.forEach(section => {
+      section.style.backgroundColor = "";
+      section.style.color = "";
+    });
+  }
+});
