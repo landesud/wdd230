7adeb560a2aabea7071480ca81eee8649a6d268d
landesud <landesud@outlook.com>
2024-11-15 20:38:29 -0600

    Links

--- scripts/links.js
+++ scripts/links.js
@@ -1,20 +1,38 @@
 const baseURL = "https://landesud.github.io/wdd230/";
 const linksURL = "https://landesud.github.io/wdd230/data/links.json";
+const weeklist = document.querySelector("#weeklist");
 
-const displayLinks = (link) => {
-  // https://byui-cse.github.io/wdd230-course/lesson09/aa-json.html
-  // I left at step 4
+const displayLinks = (weeks) => {
+  weeks.forEach((week) => {
+    let li = document.createElement('li');
+    let weeknum = week.lesson;
+
+    li.textContent = `${weeknum}: `;
+
+    week.links.forEach((link, index) => {
+      let anchor = document.createElement('a');
+      anchor.href = link.url;
+      anchor.textContent = link.title;
+      anchor.target = '_blank';
+
+      li.appendChild(anchor);
+
+      if (index < week.links.length - 1) {
+        li.appendChild(document.createTextNode(' | '));
+      }
+    });
+
+    weeklist.appendChild(li);
+  });
 }
-
-
 
 async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
 
- // displayLinks(data.lessons);
- console.table(data.lessons);;
+  // console.table(data.lessons);
+  displayLinks(data.lessons);
 }
 
+getLinks();
 
-getLinks() 
